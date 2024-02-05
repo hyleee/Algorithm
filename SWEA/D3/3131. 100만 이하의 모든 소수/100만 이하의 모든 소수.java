02a191708565fb798b489a1dd364861b905bd713
hyleee <87406368+hyleee@users.noreply.github.com>
2024-02-06 08:16:00 +0900

    [D3] Title: 100만 이하의 모든 소수, Time: 702 ms, Memory: 36,556 KB -BaekjoonHub

---
+++ SWEA/D3/3131. 100만 이하의 모든 소수/100만 이하의 모든 소수.java
@@ -0,0 +1,16 @@
+public class Solution {
+	public static void main(String[] args) {
+		for(int i=2; i<1000000; i++) {
+			int flag =0;
+			for(int j=2; j<=Math.sqrt(i); j++) {
+				if(i%j==0) {
+					flag =1;
+					break;
+				}
+			}
+			if(flag==0) { // flag==0이면 소수
+				System.out.print(i+" ");
+			}
+		}
+	}
+}
